b6f55fba8f887b6e30da60b627b589e65390e182
Hans Wärneryd <Spinigo@users.noreply.github.com>
2019-01-23 14:56:58 +0100

    minor changes

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         
     /*    swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
         
-        bgImg.frame = view.frame */
+        bgImg.frame = view.frame // Background image fills the whole screen, i.e. frame*/ 
         
         // x middle of view adjusted with width of image to get middle of image in the middle of the view
     }
