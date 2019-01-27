ab206d2c9ae1110d92d2387627ecec81195bc165
Hans Wärneryd <Spinigo@users.noreply.github.com>
2019-01-27 17:23:06 +0100

    VCLeague tapped

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Hans Wärneryd on 2019-01-27.
+//  Copyright © 2019 Hans Wärneryd. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
